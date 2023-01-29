9a08542bb63d401255d2c46c4333678929842c35
Edo Prasnikar <edo.prasnikar@gmail.com>
2023-01-29 18:45:44 +0100

    config

--- backend/src/config.js
+++ backend/src/config.js
@@ -60,7 +60,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2023-01-29T13:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2023-01-28T12:06:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 777; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x5A89caf96441C98E84e5a4225E6010eA85243b42"; // Address that will receive the royaltys
 const BASE_URI = null; // only update if you want to manually set the base uri
